634239728314eea164dd511ebdbc1aff4241b57c
ecoreng <me@ecor.me>
2014-12-04 14:05:09 -0800

    simplify build

--- src/InjectorBuilder.php
+++ src/InjectorBuilder.php
@@ -6,8 +6,15 @@ class InjectorBuilder
 {
 
     protected $di;
+    protected $registerers;
 
-    public function __construct(\Auryn\Injector $di = null)
+    public function __construct()
+    {
+        $registerers = func_get_args();
+        $this->registerers = $registerers;
+    }
+
+    public function setDi(\Auryn\Injector $di = null)
     {
         $di = $di !== null ? : new \Auryn\Provider;
         $this->di = $di;
@@ -15,12 +22,30 @@ class InjectorBuilder
 
     public function register(\Base\Interfaces\ServiceRegistererInterface $sr)
     {
+        if ($this->di === null) {
+            $this->setDi();
+        }
         $sr->register($this->di);
         return $this;
     }
-    
-    public function getDi(){
+
+    public function getDi()
+    {
         return $this->di;
+    }
+
+    public function getApp()
+    {
+        foreach ($this->registerers as $reg) {
+            $this->register($reg);
+        }
+        $di = $this->getDi();
+        return $di->make('\Base\Interfaces\AppInterface');
+    }
+
+    public function getComponents()
+    {
+        return [$this->getApp(), $this->getDi()];
     }
 
 }
